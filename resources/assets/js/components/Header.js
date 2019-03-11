9ef25bbb69ab0b74de5f301fcbdea1690fa5c046
rizkypjt <rizkyhidayatpanjaitan97@gmail.com>
2019-03-11 10:57:56 +0700

    React Router/ Router basic

--- resources/assets/js/components/Header.js
+++ resources/assets/js/components/Header.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 export default class Header extends Component {
     render() {
         return (
+            <Router>
             <div>
-                This is Header.
+                <Link to="/"> Home</Link>
+                <Link to="/about"> About Us</Link>
+
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
             </div>
+            </Router>
         );
     }
 }
--- resources/assets/js/components/Index.js
+++ resources/assets/js/components/Index.js
@@ -8,18 +8,7 @@ export default class Index extends Component {
         return (
             <div className="container">
             <Header/>
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
-
-                            <div className="card-body">
-                                I'm an example component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <Footer/>
+             <Footer/>
             </div>
         );
     }
